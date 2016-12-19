d0416c6af14b3bf22bbeac0999e8e99a6102f96d
pablozabo <pablozabo@hotmail.com>
2016-12-19 20:57:25 -0300

    Project Init

---
+++ Cresud.CDP.MainWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Cresud.CDP.MainWebApp.MvcApplication" Language="C#" %>
---
+++ Cresud.CDP.MainWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Cresud.CDP.MainWebApp.Startup))]
+namespace Cresud.CDP.MainWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
