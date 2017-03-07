6d47fd932787f6dfe871ecba3b253acb9b94e67d
pablozabo <pablozabo@hotmail.com>
2017-03-07 11:31:03 -0300

    Avances en choferes

--- Cresud.CDP.MainWebApp/Scripts/app/choferes/choferesModule.js
+++ Cresud.CDP.MainWebApp/Scripts/app/choferes/choferesModule.js
@@ -10,7 +10,8 @@
     'ngGrid',
     '$strap.directives',
     'cresud.cdp.directive.loading',    
-    'cresud.cdp.directive.debounce'    
+    'cresud.cdp.directive.debounce',
+    'cresud.cdp.directive.int'
 ]).config([
     '$routeProvider',
     '$locationProvider',
